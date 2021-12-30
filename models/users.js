87ead45a727bcd390be827032dd71624a757ba18
ahmoody2000 <58375227+ahmedmoh1234@users.noreply.github.com>
2021-12-30 23:26:09 +0200

    Added mongoose and mongoDB but still no fully implemented

---
+++ models/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema
+(
+    {
+        id : 
+        {
+            type : Int32Array,
+            required : true
+        },
+        username :
+        {
+            type : String,
+            required : true
+        },
+        password :
+        {
+            type : String,
+            required : true
+        }
+    }
+);
+
+
+const User = mongoose.model("User", userSchema);
+//Here, "User" is the collection (table) name. However, mongoose pluralise the name so it
+//will search form "Users" instead.
+
+//Export the model to use it everywhere in the project
+module.exports = User;
--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 //Require Express package
 const express = require("express");
 
+//Require mogoose for mongoDB
+const mongoose = require("mongoose");
+
 
 //Create a server from Express
 const app = express();
 
+
 //Port number to listen to
 const port = process.env.PORT || 5000;
 
-app.listen(port, (req,res)=>
+
+//Connect to mongoDB
+const dbURI = 'mongodb+srv://cinema:star123@cluster0.l3vsk.mongodb.net/cinema?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(  (result) => 
 {
-    console.log(`Listening to port ${port}`);
+    //If connected successfully, listen to the port
+    console.log("Connected to DB");
+    app.listen(port, (req,res)=>
+    {
+        console.log(`Listening to port ${port}`);
+    })
 })
+.catch( (err)    => console.log("ERROR ", err)      );
+
+
