d8cf322052ec06921a8c87d1c9d6a04092c330ab
ahmoody2000 <58375227+ahmedmoh1234@users.noreply.github.com>
2022-01-01 19:48:57 +0200

    Create reservations.js

---
+++ models/reservations.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema
+(
+    {
+        user :
+        {
+            type        : mongoose.Types.ObjectId,
+            required    : true,
+            ref         : "User"
+        },
+        movie :
+        {
+            type        : mongoose.Types.ObjectId,
+            required    : true,
+            ref         : "Movie"
+        },
+        occupiedSeats :
+        {
+            type        : [Number],
+            required    : true
+        }
+    }
+);
+
+const Reservation = mongoose.model("Reservation", reservationSchema);
+//Here, "Reservation" is the collection (table) name. However, mongoose pluralise the name so it
+//will search form "Reservations" instead in the collections.
+
+//Export the model to use it everywhere in the project
+module.exports = Reservation;
